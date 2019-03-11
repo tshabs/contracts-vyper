7e904f6feb0a431beb71982834d31468bd5f7d33
User <info@canterburycomputers.com>
2019-03-11 21:45:30 +0100

    cleanup migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,11 +7,22 @@ if(argv.testerc20){
 }
 
 module.exports = function(deployer) {
-  // first deploy exchange template
-  deployer.deploy(uniswap_exchange).then(function(){
-      return deployer.deploy(uniswap_factory, uniswap_exchange.address)
-  });
+  // deploy erc20 if needed
   if(argv.testerc20){
     deployer.deploy(uniswap_test_erc20, web3.utils.toHex(""), web3.utils.toHex(""), 18, 1000);//web3.utils.BN(18), web3.utils.BN(1000000000000000000000));
   }
+  // first deploy exchange template
+
+  deployer.deploy(uniswap_exchange);
+  deployer.deploy(uniswap_factory);
+
+  uniswap_factory.deployed(uniswap_factory).then(function(instance){
+      instance.initializeFactory(uniswap_exchange.address).then(function(tx) {
+        console.log("Initialized factory successfully. (set exchange template address to: ", uniswap_exchange.address, ")");
+        console.log(tx);
+        return deployer;
+      }).catch(function(e) {
+        console.log(e);
+      });
+  });
 };
